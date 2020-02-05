dcee34283da4cdeb6d399e2b4cc2510a54e520ec
smeggins <phillipchadwick@hotmail.com>
2020-02-04 19:16:05 -0800

    migrates front page content from front-page to home.php

--- themes/quotes/home.php
+++ themes/quotes/home.php
@@ -1,46 +1,11 @@
-<?php
-
-get_header(); ?>
+<?php get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-        <h1> Archives of Old</h1>
+				<?php get_template_part( 'template-parts/content', 'front-page' ); ?>
 
-        <?php if ( have_posts() ) : ?>
-
-            <div class="cats">
-                <h2> Quote Authors </h2>
-                <ul class="categories-container">
-                <?php wp_get_archives('type=postbypost&limit=1000'); ?>
-                </ul>
-            </div>
-
-            <div class="cats">
-                <h2> categories </h2>
-                <ul class="categories-container mobile-container">
-                    <?php wp_list_categories( 'title_li' ) ?>
-                </ul>
-            </div>
-            
-            <div class="cats">
-                <h2> tags </h2>
-                <ul class="tags-container mobile-container">
-                    <?php 
-                    $args = array('taxonomy' => 'post_tag', 'hide_empty' => true, 'title_li' => __( '' ) );
-                    wp_list_categories( $args ) 
-                    ?>
-                </ul>
-            </div>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-
-			<?php endwhile; ?>
-
-		<?php endif; ?>
-
-		</main>
-	</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
--- themes/quotes/template-parts/content-front-page.php
+++ themes/quotes/template-parts/content-front-page.php
@@ -2,7 +2,6 @@
 
 
 	<div class="entry-content">
-		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
